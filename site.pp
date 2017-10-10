a7b7f3f81f16cd011b32a401bb868e8fbb532a40
Chad Smith <chad.smith@canonical.com>
2017-10-10 16:36:04 -0600

    basic site manifest

---
+++ site.pp
@@ -0,0 +1,14 @@
+class webserver {
+  notify { 'I am a web server' }
+}
+class databaseserver {
+  notify { 'I am a database server' }
+}
+
+
+node /^web.*$/ {
+  include webserver
+}
+node /^db.*$/ {
+  include databaseserver
+}
